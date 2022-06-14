12418013f068acd1540d082a56bb909f0464e404
Jeppe Rask <jepperaskdk@gmail.com>
2022-06-14 17:13:56 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # cmake
 sudo apt update
-sudo apt install -y software-properties-common lsb-release
+sudo apt install -y git g++ software-properties-common lsb-release wget zlib1g-dev pkg-config cmake uuid-dev openjdk-11-jre
 sudo apt clean all
 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
@@ -8,15 +8,13 @@ sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) 
 sudo apt update
 sudo apt install kitware-archive-keyring
 sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
-sudo apt install cmake
-sudo apt install uuid-dev
 
 
 # LLVM-12
 printf "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-12 main" |sudo tee /etc/apt/sources.list.d/llvm-toolchain-xenial-12.list
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key |sudo apt-key add -
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt update
-sudo apt install llvm-12 libclang-12-dev
+sudo apt install llvm-12 libclang-12-dev clang-12
 
 # Antlr
 sudo apt install openjdk-11-jdk
